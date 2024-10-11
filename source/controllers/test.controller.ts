53fc2a86a55411953ee5196fdddad2dca1439046
Kat Zongker <katrina@zongker.net>
2024-10-10 22:16:18 -0500

    attemptTwo

---
+++ source/controllers/test.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+import axios, { AxiosResponse } from 'axios';
+
+export default class TestController {
+    static testFunction = async (req: Request, res: Response, next: NextFunction) => {
+        let result: AxiosResponse = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+        return res.send(result.data);
+    }
+};
