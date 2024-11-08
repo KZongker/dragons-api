30881033d05a4390fa17d36f60f1b6234220c52a
Kat Zongker <katrina@zongker.net>
2024-11-08 14:48:35 -0600

    adminDeletion

--- source/controllers/HumanController.ts
+++ source/controllers/HumanController.ts
@@ -1,4 +1,4 @@
-import { controller, httpGet, httpPost, interfaces, requestParam } from "inversify-express-utils";
+import { controller, httpDelete, httpGet, httpPost, interfaces, requestParam } from "inversify-express-utils";
 import express from "express";
 import { BaseController } from "./BaseController"
 import { Human } from "../models/Human";
@@ -26,6 +26,14 @@ export class HumanController extends BaseController {
         return this.actionWrapperAnon(req, res, async () => {
             console.log(req.body);
             return this.repos.human.save(req.body);
+        });
+    }
+
+    @httpDelete("/:id")
+    public async delete(req: express.Request<{}, {}, any>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+        return this.actionWrapperAnon(req, res, async () => {
+            console.log(req.body);
+            return this.repos.human.delete(req.body);
         });
     }
 
--- source/repos/HumanRepo.ts
+++ source/repos/HumanRepo.ts
@@ -15,6 +15,11 @@ export class HumanRepo {
         return human.humanId ? this.update(human) : this.create(human);
     }
 
+    public delete(human: Human) {
+        console.log('Deleted!')
+        return this.deletion(human);
+    }
+
     private async create(human: Human) {
         const sql = "INSERT INTO `humans` (firstName, middleName, lastName, dragon, age, bravery, intelligence, speed, attack, defense, color, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
         const params = [human.firstName, human.middleName, human.lastName, human.dragon, human.age, human.bravery, human.intelligence, human.speed, human.attack, human.defense, human.color, human.image];
@@ -28,5 +33,11 @@ export class HumanRepo {
         const params = [human.firstName, human.middleName, human.lastName, human.dragon, human.age, human.bravery, human.intelligence, human.speed, human.attack, human.defense, human.color, human.image, human.humanId];
         await DB.query(sql, params);
         return human;
+    }
+
+    private async deletion(human: Human) {
+        const sql = "DELETE FROM `humans` WHERE humanId=?";
+        const params = [human.humanId];
+        await DB.query(sql, params);
     }
 }
